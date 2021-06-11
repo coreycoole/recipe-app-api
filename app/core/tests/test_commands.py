3d7e0ec4e1de0d2787bec356628ec9fca679e1ff
Corey Coole <coreycoole@MacBook-Pro.local>
2021-06-10 20:42:17 -0700

    code clean up from checker

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,10 +9,10 @@ class CommandTests(TestCase):
 
     def test_wait_for_db_ready(self):
     """Test waiting for db when db is available"""
-    with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-        gi.return_value = True
-        call_command('wait_for_db')
-        self.assertEqual(gi.call_count, 1)
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
 
 
     @patch('time.sleep', return_value=True)
