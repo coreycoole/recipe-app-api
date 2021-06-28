66483a79da1bb3e581aeb32bcf91a9415621d1a3
Corey Coole <coreycoole@MacBook-Pro.local>
2021-06-27 21:54:10 -0700

    more clean up

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -132,7 +132,7 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
             'name': self.user.name,
-            'email': self.user.email
+            'email': self.user.email,
         })
 
         def test_post_me_not_aloowed(self):
